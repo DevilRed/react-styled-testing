f31abfa668e13a5810a579d07abca4a1f2a5ed67
Christian <chrisdaq@gmail.com>
2019-09-10 15:18:39 -0400

    use styled components in App component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
 import React from 'react';
+import styled from 'styled-components'
+import Header from './components/Header';
+import Profile from './components/Profile.js';
+import GlobalStyle from './theme/globalStyle';
 
 function App() {
   return (
-    <div className="App">
-      jojo
-    </div>
+    <React.Fragment>
+      <GlobalStyle />
+      <div>
+        <Header />
+        <Profile />
+      </div>
+    </React.Fragment>
   );
 }
 
